13a6e3df3d4eda1c90b7f166302af07e13fe75a8
Nitin <nitin.nimesh789@gmail.com>
2021-12-30 15:18:55 +0530

    complete backed functionality

--- src/main/java/com/PaymentGateWay.java
+++ src/main/java/com/PaymentGateWay.java
@@ -2,6 +2,10 @@ package com;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.ServletException;
@@ -27,18 +31,51 @@ public class PaymentGateWay extends HttpServlet {
 		
 		
 		String customername = request.getParameter("customername");
-		String contactno = request.getParameter("contactno");
+		int contactno = Integer.parseInt( request.getParameter("contactno"));
 		String email = request.getParameter("email");
 		String Address = request.getParameter("Address");
+		
+		
+		
 		HttpSession session=request.getSession(false);  
         int seat=(int) session.getAttribute("seat"); 
         int Price=(int)session.getAttribute("Price"); 
+        int Schedule_id=(int)session.getAttribute("schedule");
         int Seat_Availble=(int)session.getAttribute("Seat_Availble");
         int total_price =Price*seat;
+        int seat_reserve =Seat_Availble-seat;
+        
+        
+        
 	     PrintWriter out=response.getWriter();
 	     out.println("Name "+customername+"Contact NO."+contactno+"Mail"+email+"Adddress"
 	     +Address+"Your booked setas"+seat+"Total price"+total_price+"Seat avaialbe"+Seat_Availble);
+	    
+	 	try{  
+			Class.forName("com.mysql.cj.jdbc.Driver");  
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/flyaway","root","root");  
+			  	
+			PreparedStatement stmt1=con.prepareStatement("update schedule set seat=? where sch_id=?;");  
+			stmt1.setInt(1, seat_reserve);
+			stmt1.setInt(2, Schedule_id);
+			int rs=stmt1.executeUpdate();
+			out.println(rs+" Your Seat has been Successfully Booked!!");  
+			
+			PreparedStatement stmt=con.prepareStatement("insert into airline_booked(schedule_id, customer_name , contact_no, email , seat_booked,paid  ) values(?,?,?,?,?,?);");
+			stmt.setInt(1, Schedule_id);
+			stmt.setString(2, customername);
+			stmt.setInt(3, contactno);
+			stmt.setString(4, email);
+			stmt.setInt(5, seat);
+			stmt.setInt(6, total_price);
+			
+			int i=stmt.executeUpdate();
+		out.println(i+" We have received Payment Thanks for payment ");  
+					
+					con.close();  
+		}catch(Exception e){ out.println(e);}
 	     
+	   
 	     
 	}
 
